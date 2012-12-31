2f7bb39c268fecbff049fa7b125f747913831723
Will Goto <will.r.goto@gmail.com>
2012-12-31 10:57:01 -0500

    Add sidebar functionality

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,2 +1,17 @@
 class SiteController < ApplicationController
+  def about_us
+    @page_title = "About Us"
+  end
+  
+  def presidents_message
+    @page_title = "President's Message"
+  end
+  
+  def mission_statement
+    @page_title = "Mission Statement"
+  end
+  
+  def global_supply
+    @page_title = "Global Supply"
+  end
 end
